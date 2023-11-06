22b8cf63c58fc5b8bf9afefe5e711ad71621c441
YurijBielousovDnepr <gakayur@gmail.com>
2023-11-06 22:33:26 +0200

    06-11 comit 1 fix моделей

--- crm/models.py
+++ crm/models.py
@@ -5,6 +5,7 @@ from booking_cayak.models import MyUser
 class BookingHistory(models.Model):
     status = models.CharField(max_length=1, choices=[('C', 'В обробці'), ('B', 'Затверджено'), ('R', 'Відхилено')],
                               verbose_name="Статус бронювання")
-    User_ = models.ForeignKey(MyUser, on_delete=models.CASCADE, verbose_name="Користувач")
+    rent_date = models.DateField(verbose_name="Дата прокату")
+    User_History = models.ForeignKey(MyUser, on_delete=models.CASCADE, verbose_name="Користувач")
     start_time = models.DateTimeField(verbose_name="Час початку")
     end_time = models.DateTimeField(verbose_name="Час закінчення")
