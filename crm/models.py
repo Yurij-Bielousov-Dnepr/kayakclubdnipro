75d864964d6da61d15cc185633a210f6f44450b0
YurijBielousovDnepr <gakayur@gmail.com>
2023-11-29 23:56:53 +0200

    29-11 comit 1  правка html+models

--- crm/models.py
+++ crm/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.urls import reverse
 from booking_cayak.constants import BOAT_TYPES, RATING_CHOICES, DURATION_CHOICES, TAG_CHOICES
+from booking_cayak.models import BoatStatus, Boat
 
 
 
