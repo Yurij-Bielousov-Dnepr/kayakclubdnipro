5649bc40d90353a51adfb8c1d60b0d5f7320e7a3
YurijBielousovDnepr <gakayur@gmail.com>
2023-12-30 23:27:42 +0200

    30-12 comit 1 правка Index.html +models

--- kayakclubdnipro/settings.py
+++ kayakclubdnipro/settings.py
@@ -75,8 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "kayakclubdnipro.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Database  https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
