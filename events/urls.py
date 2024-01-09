aac056ccb77d15167cd86ee563d4664887d90c54
YurijBielousovDnepr <gakayur@gmail.com>
2024-01-09 23:28:38 +0200

    09-01-24 comit 1 правка html +models

--- events/urls.py
+++ events/urls.py
@@ -12,11 +12,15 @@ from django.conf.urls.static import static
 from django.contrib.admin.views.decorators import staff_member_required
 from . import views
 from .views import Events_detail
+from .views import event_list, event_detail, create_event, edit_event
+
 
 app_name = "art_event"  # добавьте это, если используете пространства имен
-
-
 urlpatterns = [
+    path('events/', event_list, name='event_list'),
+    path('events/<int:pk>/', event_detail, name='event_detail'),
+    path('events/create/', create_event, name='create_event'),
+    path('events/<int:pk>/edit/', edit_event, name='edit_event'),
     path('events/<int:pk>/', Events_detail.as_view(), name='event_detail'),
     path("events/add/", views.EventCreateView.as_view(), name="add_event"),
     path(
@@ -29,8 +33,8 @@ urlpatterns = [
         views.EventDeleteView.as_view(),
         name="delete_event",
     ),
-]
 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
